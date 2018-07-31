218f37fd1d91ca4724e7eaaf0f1dfc6e86860282
VasiliyZhigalov <mailto:vasiliyzhigalov@gmail.com>
2018-07-31 22:58:07 +0400

    1.4. JUnit

---
+++ chapter_001/src/test/java/ru/job4j/calculate/CalculateTest.java
@@ -0,0 +1,27 @@
+package ru.job4j.calculate;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+* Test.
+*
+* @author Vasiliy Zhigalov (mailto:vasiliyzhigalov@gmail.com)
+* @version $Id$
+* @since 0.1
+*/
+public class CalculateTest {
+	/**
+	* Test echo.
+	*/ 
+	@Test
+	public void whenTakeNameThenTreeEchoPlusName() {
+    String input = "Vasiliy Zhigalov";
+    String expect = "Echo, echo, echo : Vasiliy Zhigalov"; 
+    Calculate calc = new Calculate();
+    String result = calc.echo(input);
+    assertThat(result, is(expect));
+}
+ 
+}
