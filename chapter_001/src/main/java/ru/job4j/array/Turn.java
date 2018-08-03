9c9119e229aa83aed64ae9c8b5d1656bca58e9be
VasiliyZhigalov <mailto:vasiliyzhigalov@gmail.com>
2018-08-03 15:45:20 +0400

    6.2. Перевернуть массив. [#4441] Одного метода достаточно для четных и нечетных массивов.

--- chapter_001/src/main/java/ru/job4j/array/Turn.java
+++ chapter_001/src/main/java/ru/job4j/array/Turn.java
@@ -5,8 +5,9 @@ public class Turn {
         int tmp;
         for (int i = 0; i < array.length / 2; i++) {
             tmp = array[i];
-            array[i]=array[array.length-1-i];
-            array[array.length-1-i]=tmp;
+
+            array[i] = array[array.length - 1 - i];
+            array[array.length - 1 - i] = tmp;
         }
         return array;
     }
