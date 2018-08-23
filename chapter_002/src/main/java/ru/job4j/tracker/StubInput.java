4eadfd879f03927f5aa29c7e9c9a5fcda46f862f
VasiliyZhigalov <mailto:vasiliyzhigalov@gmail.com>
2018-08-23 19:48:17 +0400

    3. if-else-throw [#49895]

--- chapter_002/src/main/java/ru/job4j/tracker/StubInput.java
+++ chapter_002/src/main/java/ru/job4j/tracker/StubInput.java
@@ -11,20 +11,9 @@ public class StubInput implements Input {
     public String ask(String question) {
         return this.answers[this.position++];
     }
-
     public int ask(String question, int[] range) {
         int key = Integer.valueOf(this.ask(question));
-        boolean exist = false;
-        for (int value : range) {
-            if (value == key) {
-                exist = true;
-                break;
-            }
-        }
-        if (exist) {
-            return key;
-        } else {
-            throw new MenuOutException("Out of menu range");
-        }
+        new ValidateInput(this).notInTheRange(key, range);
+        return key;
     }
 }
