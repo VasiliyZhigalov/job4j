02f10ca03ee481ea6adf30c1e0a745b79e13d0d7
VasiliyZhigalov <mailto:vasiliyzhigalov@gmail.com>
2018-10-11 15:40:53 +0400

    2. Написать программу преобразования List в Map. [#10093]

---
+++ chapter_003/src/test/java/ru/job4j/list/UserConvertTest.java
@@ -0,0 +1,25 @@
+package ru.job4j.list;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class UserConvertTest {
+    @Test
+    public void whenUserListThenUserMap() {
+        List<User> UserList = Arrays.asList(
+                new User(1, "Vasiliy", "Izhevsk"),
+                new User(2, "Ivan", "Ivanovo")
+        );
+        UserConvert userConvert = new UserConvert();
+        HashMap<Integer, User> result = userConvert.process(UserList);
+
+        assertThat(result.get(2).name, is("Ivan"));
+    }
+}
