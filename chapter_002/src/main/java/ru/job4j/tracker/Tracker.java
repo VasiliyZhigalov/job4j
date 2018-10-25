8b5b13fdcd0deb4e40ddcff955af437f2810869e
VasiliyZhigalov <mailto:vasiliyzhigalov@gmail.com>
2018-10-25 08:30:59 +0400

    3. В задании Трекере в классе Tracker переделать поиск на lambda.+ srteam API

--- chapter_002/src/main/java/ru/job4j/tracker/Tracker.java
+++ chapter_002/src/main/java/ru/job4j/tracker/Tracker.java
@@ -4,13 +4,13 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class Tracker {
     /**
      * Массив для хранение заявок.
      */
     private final List<Item> items = new ArrayList<>();
-    private List<String> id = new ArrayList<>();
 
     /**
      * Метод реализаущий добавление заявки в хранилище
@@ -42,25 +42,14 @@ public class Tracker {
      */
     public boolean replace(String id, Item item) {
         boolean result = false;
-        int index = this.findIndexById(id);
-        if (index != -1) {
+        Item replaceItem = this.findById(id);
+        if (replaceItem != null) {
+            int index = items.indexOf(replaceItem);
             items.set(index, item);
             items.get(index).setId(id);
             result = true;
         }
         return result;
-    }
-
-    /**
-     * сравнение id
-     *
-     * @param id
-     * @param item
-     * @return
-     */
-    private boolean comparID(String id, Item item) {
-        Predicate<String> predicate = idF -> item.getId().equals(idF);
-        return predicate.test(id);
     }
 
     /**
@@ -70,8 +59,7 @@ public class Tracker {
      * @return
      */
     public Item findById(String id) {
-        int index = findIndexById(id);
-        return index == -1 ? null : items.get(index);
+        return items.stream().filter(item -> item.getId().equals(id)).findAny().orElse(null);
     }
 
     /**
@@ -81,32 +69,8 @@ public class Tracker {
      * @return
      */
     public List<Item> findByName(String key) {
-        List<Item> itemByName = new ArrayList<>();
-        for (Item item : items) {
-            if (key.equals(item.getName())) {
-                itemByName.add(item);
-            }
-        }
+        List<Item> itemByName = items.stream().filter(item -> item.getName().equals(key)).collect(Collectors.toList());
         return itemByName;
-    }
-
-    /**
-     * Ищет индекс по id
-     *
-     * @param id
-     * @return
-     */
-    private int findIndexById(String id) {
-        int result = -1;
-        int index = 0;
-        for (Item item : items) {
-            if (comparID(id, item)) {
-                result = index;
-                break;
-            }
-            index++;
-        }
-        return result;
     }
 
     /**
@@ -116,9 +80,9 @@ public class Tracker {
      */
     public boolean delete(String id) {
         boolean result = false;
-        int index = this.findIndexById(id);
-        if (index != -1) {
-            items.remove(index);
+        Item delItem = findById(id);
+        if (delItem != null) {
+            items.remove(delItem);
             result = true;
         }
         return result;
